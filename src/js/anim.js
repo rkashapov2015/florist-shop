54b56a0a6849cab4277fa7a0a8ef1794f81149fc
Rinat Kashapov <rinatkzz@yandex.ru>
2018-02-01 14:48:10 +0300

    small fixes on the work time

--- src/js/anim.js
+++ src/js/anim.js
@@ -16,7 +16,49 @@ function tick() {
     window.requestAnimationFrame(tick);
 }
 
-function createCross(x, y, size, rotate) {
+function createTriangle(x, y, size, rotate, color) {
+    size = size*2;
+    //0,00	10,00
+    //-8,68	-4,96
+    //8,62	-5,08
+    ctx.save();
+    ctx.beginPath()
+    ctx.translate(x,y);
+    ctx.rotate(rotate);
+    ctx.moveTo(0, 10*size);
+    ctx.lineTo(-8.68*size, -4.96*size);
+    ctx.lineTo(8.62*size, -5.08*size);
+    ctx.lineTo(0, 10*size);
+    ctx.lineTo(-8.68*size, -4.96*size);
+    ctx.lineWidth = 2 * size;
+    if (!color) {
+        color = '#ffffff';
+    }
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+}
+
+function createSquare(x, y, size, rotate, color) {
+    var length = 30 * size;
+    ctx.save();
+    ctx.beginPath()
+    ctx.translate(x,y);
+    ctx.rotate(rotate);
+    
+    ctx.rect(-length/2, -length/2, length, length);
+    ctx.lineWidth = 1 * size;
+    if (!color) {
+        color = '#ffffff';
+    }
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+}
+
+function createCross(x, y, size, rotate, color) {
     var length = 20 * size;
     ctx.save();
     ctx.beginPath();
@@ -27,13 +69,16 @@ function createCross(x, y, size, rotate) {
     ctx.moveTo(0, -length/2);
     ctx.lineTo(0,length/2);
     ctx.lineWidth = 5 * size;
-    ctx.strokeStyle = '#ffffff';
+    if (!color) {
+        color = '#ffffff';
+    }
+    ctx.strokeStyle = color;
     ctx.stroke();
     ctx.closePath();
     ctx.restore();
 }
 
-function createStar(x, y, size, rotate) {
+function createStar(x, y, size, rotate, color) {
     var length = 20 * size;
     //0,00	10
     //-9,51	3,09
@@ -56,31 +101,32 @@ function createStar(x, y, size, rotate) {
     ctx.moveTo(0,(10*size));
     
     ctx.lineTo(-(5.88*size),-(8.09*size));
-
     ctx.lineTo(9.51*size,3.09*size);
-
     ctx.lineTo(-(9.51*size),3.09*size);
-
     ctx.lineTo((5.88*size),-(8.09*size));
-
     ctx.lineTo(0,(10*size));
-
     ctx.lineTo(-(5.88*size),-(8.09*size));
 
     ctx.lineWidth = 5 * size;
-    ctx.strokeStyle = '#ffffff';
+    if (!color) {
+        color = '#ffffff';
+    }
+    ctx.strokeStyle = color;
     ctx.stroke();
     ctx.closePath();
     ctx.restore();
 }
   
-function createArc(x, y, size) {
+function createArc(x, y, size, color) {
     ctx.save();
     ctx.beginPath();
     ctx.translate(x,y);
     ctx.arc(0, 0, 12*size, 0, 2*Math.PI, false);
     ctx.lineWidth = 5 * size;
-    ctx.strokeStyle = '#ffffff';
+    if (!color) {
+        color = '#ffffff';
+    }
+    ctx.strokeStyle = color;
     ctx.closePath();
     ctx.stroke();
     ctx.restore();
@@ -93,22 +139,36 @@ function initPaint() {
         var r = randomMinMax(0,2*PI, 100);
         var type = '';
         var size = randomMinMax(0.1,0.6, 100);
-        if(Math.floor(Math.random() * 100) > 50) {
-          //createCross(x, y, size, r);
-          createStar(x,y,size, r);
-          type = 'cross';
+        var rndValue = Math.floor(Math.random() * 100);
+        var hue = randomMinMax(0, 359, 1);
+        var color = 'hsl(' + hue + ',100%,50%)';
+        if (rndValue < 20) {
+            createStar(x,y,size, r, color);
+            type = 'star';
+        } else if (rndValue < 40) {
+            createArc(x, y, size, color);
+            type = 'arc';
+        } else if (rndValue < 60) {
+            createCross(x, y, size, r, color);
+            type = 'cross';
+        } else if (rndValue < 80) {
+            createSquare(x, y, size, r, color);
+            type = 'square';
         } else {
-          createArc(x, y, size);
-          type = 'arc';
+            createTriangle(x, y, size, r, color);
+            type = 'triangle';
         }
+
         var angularVelocity = randomMinMax(-0.2,0.2, 1000);
+        
         array[i] = {
           x: x, 
           y: y, 
           r: r,
           size: size,
           ang: angularVelocity, 
-          type: type, 
+          type: type,
+          color: color,
           func: Math.floor(Math.random() * 100) > 50?nextPointOne:nextPointTwo
         };
     }
@@ -120,15 +180,26 @@ function repaint() {
     for (var i = 0; i< array.length; i++) {
         var object = array[i];    
         var point = object.func(object.x, object.y, Date.now());
-        if (object.type === 'cross') {
+        
             object.r += object.ang;
             if (object.r > 2* PI) object.r = 0;
             if (object.r < 0) object.r = 2*PI;
-            //createCross(point.x,point.y, object.size, object.r);
-            createStar(point.x,point.y,object.size, object.r);
-        } else {
-            createArc(point.x,point.y, object.size);  
-        }
+            switch(object.type) {
+                case 'cross':
+                createCross(point.x,point.y, object.size, object.r, object.color);
+                break;
+                case 'star':
+                createStar(point.x,point.y,object.size, object.r, object.color);
+                break;
+                case 'square':
+                createSquare(point.x, point.y, object.size, object.r, object.color);
+                break;
+                case 'triangle':
+                createTriangle(point.x, point.y, object.size, object.r, object.color);
+                break;
+                default: 
+                createArc(point.x,point.y, object.size, object.color);      
+            }
     }
 }
   
